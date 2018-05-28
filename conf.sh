0bb6fdc65590aebacf515d0176b8d1202e540db7
SergeRylko <serge.rylko@mobexs.com>
2018-05-28 17:31:31 +0300

    initial

---
+++ conf.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Configuration file used to create new project using flipper script
+
+# poject setup
+PROJECT_NAME="Seal"
+PROD_TARGET_NAME="Seal"
+BUNDLE_ID_PROD="com.forest.seal"
+BUNDLE_ID_STAGING="com.forest.seal-test"
+DEPLOYMENT_TARGET=11.0
+
+# mobexs team setup
+DEVPORTAL_LOGIN="serge.rylko@mobexs.com"
+ITUNES_CONNECT_LOGIN="serge.rylko@mobexs.com"
+DEVELOPMENT_TEAM_ID="Y2V6NG4VN2"
+ITUNES_CONNECT_TEAM_ID="38823829"
+FASTLANE_CREDENTIALS_URL="http://git.mobexs.com:8081/flipper/app-credentials.git"
+
+# modules setup
+USE_COREDATA="none" #"default" to add coredata default module
+USE_REALM="none"
